78ca14670b9007f826bc4038e526c8e164f5170c
aanyan <a.anyan@zoovu.com>
2021-08-11 14:57:16 +0200

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,119 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bold">
+          Qwitter
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer :width="283" show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- left drawer content -->
+            <q-icon name="fas fa-dove" size="lg"
+            color="primary"
+            class="q-pa-sm"/>
+
+            <q-list>
+              <q-item clickable v-ripple to="/">
+        <q-item-section avatar>
+          <q-icon name="home" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple to="/about">
+        <q-item-section avatar>
+          <q-icon name="help" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+      </q-item>
+            </q-list>
+
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- right drawer content -->
+      <q-input
+      placeholder="Search"
+      dense
+      class="q-ma-md" 
+      outlined rounded >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list separator padding>
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-grey">Education</q-item-label>
+          <q-item-label class="text-weight-bold">Virtual Learning Discussion</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-grey">Education</q-item-label>
+          <q-item-label class="text-weight-bold">Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-grey">Education</q-item-label>
+          <q-item-label class="text-weight-bold">Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
